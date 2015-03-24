8511914da981b49ae78f2d1224583019fc4209bf
Junya Kaneko <jyuneko@gmail.com>
2015-03-24 19:29:35 +0900

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='modmps',
+    version='0.0.1',
+    packages=['modmps', 'modmps.http', 'modmps.http.oauth2', 'modmps.http.oauth2.google', 'modmps.http.api_executor'],
+    url='www.mpsamurai.org',
+    license='GNU General Public License version 3',
+    author='Junya Kaneko, Akinori Hayashi',
+    author_email='junya@mpsamurai.org, akinori@mpsamurai.org',
+    description='Modules used in applications developed by Morning Project Samurai.'
+)
