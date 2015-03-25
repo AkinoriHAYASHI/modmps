1703ce414cd5344612e431a1255d1f271f672a79
Junya Kaneko <jyuneko@gmail.com>
2015-03-25 17:03:07 +0900

    Added modmps.http.web_service_client.google.plus.__init__.py.

---
+++ modmps/http/web_service_client/google/plus/__init__.py
@@ -0,0 +1,3 @@
+__author__ = 'Junya Kaneko <junya@mpsamurai.org>'
+
+from modmps.http.web_service_client.com.google.googleapis.www.plus.v1.people import PeopleApiExecutor as GooglePlusPeopleApiExecutor
